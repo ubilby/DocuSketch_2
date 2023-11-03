6c943be4d1211e6b89db0327d4c5ff8354e17bdb
ubilby <ubilby@gmail.com>
2023-11-03 17:03:15 +0400

    add db

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
+def get_db():
+    client = MongoClient(
+        host='mongo',
+        port=27017,
+        username='root',
+        password='pass',
+        authSource='admin'
+    )
+    db = client.records
+    return db
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    db = get_db()
+    values = db.values
+    values.insert_one({"test": "test", })
+    count = values.count_documents({})
+    return f"<p>{count}</p>"
