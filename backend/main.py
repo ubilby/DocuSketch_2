9d23a271647305eae921419da4bf2a8764e08727
ubilby <ubilby@gmail.com>
2023-11-04 12:13:47 +0400

    add empty methods

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,59 @@
+from typing import Callable, Optional
+
 from flask import Flask, request
 from pymongo import MongoClient
+
+
+DB_HOST = 'mongo',
+DB_PORT = 27017
+DB_USERNAME = 'root'
+DB_PASSWORD = 'pass'
+DB_NAME = 'records'
+TABLE_NAME = 'values'
 
 app = Flask(__name__)
 
 
-def get_db():
+def get_table():
     client = MongoClient(
-        host='mongo',
-        port=27017,
-        username='root',
-        password='pass',
+        host=DB_HOST,
+        port=DB_PORT,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
         authSource='admin'
     )
-    db = client.records
-    return db
+    db = client[DB_NAME]
+    table = db[TABLE_NAME]
+    return table
 
 
-@app.route("/")
-def hello_world():
-    db = get_db()
-    values = db.values
-    values.insert_one({"test": "test", })
+def get_value(value: dict) -> None:
+    return 'GET'
+
+
+def add_value(value: dict) -> None:
+
+    return 'POST'
+
+
+def edit_or_add_value(value: dict) -> None:
+
+    return 'PUT'
+
+
+def get_action(method: str) -> Optional[Callable]:
+    methods = {
+        'POST': add_value,
+        'GET': get_value,
+        'PUT': edit_or_add_value
+    }
+    return methods.get(method)
+
+
+@app.route('/', methods=['POST', 'GET', 'PUT'])
+def handle_requests():
+    values = get_table()
+    values.insert_one({'test': 'test', })
     count = values.count_documents({})
-    return f"<p>{count}</p>"
+    data = request.get_json()
+    return get_action(request.method)(data)
